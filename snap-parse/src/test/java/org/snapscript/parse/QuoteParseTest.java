2118f9e93f360538491d7fc6306b8ab45e41a949
Niall Gallagher <gallagher_niall@yahoo.com>
2015-10-08 21:28:59 +0100

    Fix new compressor

--- snap-parse/src/test/java/org/snapscript/parse/QuoteParseTest.java
+++ snap-parse/src/test/java/org/snapscript/parse/QuoteParseTest.java
@@ -5,118 +5,161 @@ import junit.framework.TestCase;
 public class QuoteParseTest extends TestCase {
    
    public void testParse() throws Exception {
-      assertEquals(new Compressor("\"c:\\\\\"".toCharArray()).quote(), 6); // "c:\\"
-      assertEquals(new Compressor(new char[]{'"', 'c', ':', '\\', '\\', '\\', '"', '"'}).quote(), 8); // "c:\\\""
-      assertEquals(new Compressor(new char[]{'"', '"'}).quote(), 2); // ""
-      assertEquals(new Compressor(new char[]{'\'', '\''}).quote(), 2); // ''
-      assertEquals(new Compressor("/** // */".toCharArray()).comment(), 9); // /** // */
-      assertEquals(new Compressor("/** // *".toCharArray()).comment(), -1); // /** // *
-      assertEquals(new Compressor("/** // *".toCharArray()).comment(), -1); // /** // *
-      assertEquals(new Compressor("/* abc */ // another one".toCharArray()).comment(), 9); // /* abc */
-      assertEquals(new Compressor("/// this is a comment".toCharArray()).comment(), 21); /// this is a comment
-      assertEquals(new Compressor("/// this is a comment\n".toCharArray()).comment(), 22); /// this is a comment
-      assertEquals(new Compressor("/// this is a comment\n\n".toCharArray()).comment(), 22); /// this is a comment
+      assertEquals(new Compressor("\"c:\\\\\"".toCharArray()).compress(), "\"c:\\\\\""); // "c:\\"
+      assertEquals(new Compressor(new char[]{'"', 'c', ':', '\\', '\\', '\\', '"', '"'}).compress(), new String(new char[]{'"', 'c', ':', '\\', '\\', '\\', '"', '"'})); // "c:\\\""
+      assertEquals(new Compressor(new char[]{'"', '"'}).compress(), new String(new char[]{'"', '"'})); // ""
+      assertEquals(new Compressor(new char[]{'\'', '\''}).compress(), new String(new char[]{'\'', '\''})); // ''
+      assertEquals(new Compressor("/** // */".toCharArray()).compress(), ""); // /** // */
+      //assertEquals(new Compressor("/** // *".toCharArray()).compress(), -1); // /** // *
+      //assertEquals(new Compressor("/** // *".toCharArray()).compress(), -1); // /** // *
+      assertEquals(new Compressor("/* abc */ // another one".toCharArray()).compress(), ""); // /* abc */
+      assertEquals(new Compressor("/// this is a comment".toCharArray()).compress(), ""); /// this is a comment
+      assertEquals(new Compressor("/// this is a comment\n".toCharArray()).compress(), ""); /// this is a comment
+      assertEquals(new Compressor("/// this is a comment\n\n".toCharArray()).compress(), ""); /// this is a comment
    }
-   private static enum Error {
-      QUOTE("Quote not terminated"),
-      COMMENT("Comment not terminated");
-      
-      private final String message;
-      
-      private Error(String message) {
-         this.message = message;
-      }
-   }
+
    private static class Compressor {
       
+      private short[] lines;
       private char[] source;
-      private int off;
+      private short line;
+      private int write;
+      private int read;
       private int count;
       
       public Compressor(char[] source) {
+         this.lines = new short[source.length];
          this.count = source.length;
          this.source = source;
       }
       
-      public int comment() {
-         char start = source[off];
-         int pos = off + 1;
-         
-         if(start == '/' && pos + 1 < count) {
-            char next = source[pos];
+      private String compress() {
+         while(read < count) {
+            char next = source[read];
             
-            if(next == '/') {
-               int size = 1;
-               
-               while(pos < count) {
-                  char terminal = source[pos];
-                  
-                  if(terminal == '\n') {
-                     return size + 1;
-                  }
-                  size++;
-                  pos++;
+            if(comment(next)) {
+               if(!comment()) {
+                  throw new IllegalStateException("Comment not complete");
                }
-               return size; // end of source
-            } 
-            if(next == '*') {
-              int size = 1;
-               
-               while(pos < count) {
-                  char terminal = source[pos];
-                  
-                  if(terminal == '/') {
-                     char prev = source[pos - 1];
-                     
-                     if(prev == '*') {
-                        return size + 1;
-                     }
-                  }
-                  size++;
-                  pos++;
+            } else if(quote(next)) {
+               if(!string()){
+                  throw new IllegalStateException("Quote not complete");
                }
+            } else if(!space(next)) {
+               lines[write] = line;
+               source[write++] = source[read++];
+            } else {
+               if(write > 0 && read + 1< count) {
+                  char before = source[write-1];
+                  char after = source[read-1];
+                  
+                  if(identifier(before) && identifier(after)) {
+                     lines[write] = line;
+                     source[write++] = ' ';               
+                  }
+                  if(operator(before) && operator(after)) {
+                     lines[write] = line;
+                     source[write++] = ' ';               
+                  }               
+               }
+               read++;
             }
-            return -1; // error
          }
-         return 0;
+         return new String(source, 0, write);
       }
       
-      public int quote() {
-         char start = source[off];
-         int pos = off + 1;
+      private boolean comment() {
+         char start = source[read];
          
-         if(start == '"' || start == '\'') {
-            int size = 1;
+         if(comment(start)){
+            if(read + 1 < count) {   
+               char next = source[read + 1];
+               
+               if(next == '/') {
+                  while(read < count) {
+                     char terminal = source[read];
+                     
+                     if(terminal == '\n') {
+                        line++;
+                        return true;
+                     }
+                     read++;
+                  }
+                  return true; // end of source
+               } 
+               if(next == '*') {
+                  while(read < count) {
+                     char terminal = source[read];
+                     
+                     if(terminal == '\n') {
+                        line++;
+                     }
+                     if(terminal == '/' && read > 0) {
+                        char prev = source[read - 1];
+                        
+                        if(prev == '*') {
+                           read++;
+                           return true;
+                        }
+                     }
+                     read++;
+                  }
+               }
+            }
+         }
+         return false;
+      }
+      
+      private boolean string() {
+         char start = source[read];
+         
+         if(quote(start)) {
+            int size = 0;
             
-            while(pos < count) {
-               char next = source[pos];
+            while(read < count) {
+               char next = source[read];
                
                if(next == start) {
                   if(size == 1) { // "" or ''
-                     return size + 1; 
+                     return true; 
                   } 
-                  char prev = source[pos - 1];
-                  
-                  if(prev != '\\') {
-                     return size + 1;
-                  }
-                  for(int i = 1; i < size; i++) {
-                     char escape = source[pos - i];
+                  if(read > 0) {
+                     char prev = source[read - 1];
                      
-                     if(escape != '\\') {
-                        if(i % 2 == 1) { 
-                           return size + 1;
+                     if(!escape(prev)) {
+                        source[write++] = source[read++];
+                        return true;
+                     }
+                     for(int i = 1; i < size; i++) {
+                        char value = source[read - i];
+                        
+                        if(!escape(value)) {
+                           if(i % 2 == 1) {
+                              source[write++] = source[read++];
+                              return true;
+                           }
+                           break;
                         }
-                        break;
                      }
                   }
                }
+               source[write++] = source[read++];
                size++;
-               pos++;
             }
-            return -1; // error
          }
-         return 0;
+         return false;
+      }
+      
+      private boolean escape(char value) {
+         return value == '\\';
+      }
+      
+      private boolean quote(char value) {
+         return value == '"' || value == '\'';
+      }
+      
+      private boolean comment(char value) {
+         return value == '/';
       }
       
       private boolean space(char value) {
