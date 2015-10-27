1e913dbb996ef00744d393d642df3fe55c64db1a
Niall Gallagher <gallagher_niall@yahoo.com>
2015-10-27 23:54:35 +0000

    Remove listener

--- snap-assemble/src/main/java/org/snapscript/assemble/AssemblerListener.java
+++
@@ -1,7 +0,0 @@
-package org.snapscript.assemble;
-
-import org.snapscript.assemble.binary.OperationCode;
-
-public interface AssemblerListener {
-   void assemble(String name, OperationCode code, Instruction instruction, Integer count, Object value);
-}
