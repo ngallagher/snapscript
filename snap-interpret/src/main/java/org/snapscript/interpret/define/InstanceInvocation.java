a7239a3e172b880a2e8e23316412216108167240
Niall Gallagher <gallagher_niall@yahoo.com>
2015-11-14 02:31:02 +0000

    Fix instance invocation

--- snap-interpret/src/main/java/org/snapscript/interpret/define/InstanceInvocation.java
+++ snap-interpret/src/main/java/org/snapscript/interpret/define/InstanceInvocation.java
@@ -1,80 +1,54 @@
 package org.snapscript.interpret.define;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.List;
 
 import org.snapscript.core.Invocation;
-import org.snapscript.core.Module;
 import org.snapscript.core.Reference;
 import org.snapscript.core.Result;
 import org.snapscript.core.Scope;
 import org.snapscript.core.Signature;
+import org.snapscript.core.SignatureAligner;
 import org.snapscript.core.Statement;
 import org.snapscript.core.Type;
+import org.snapscript.interpret.ConstraintChecker;
 
 public class InstanceInvocation implements Invocation<Scope> {
 
+   private final ConstraintChecker checker;
+   private final SignatureAligner aligner;
    private final Signature signature;
    private final Statement statement;
-   private final String name;
+   private final String function;
    
-   public InstanceInvocation(Statement statement, Signature signature, String name) {
+   public InstanceInvocation(Statement statement, Signature signature, String function) {
+      this.aligner = new SignatureAligner(signature);
+      this.checker = new ConstraintChecker();
       this.statement = statement;
       this.signature = signature;
-      this.name = name;
+      this.function = function;
    }
    
    @Override
    public Result invoke(Scope scope, Scope object, Object... list) throws Exception {
       List<String> names = signature.getNames();
       List<Type> types = signature.getTypes();
-      int modifiers = signature.getModifiers();
-      
-      if((modifiers & 0x00000080) != 0) {
-         int length = names.size();
-         int start = length - 1;
-         int remaining = list.length - start;
-         
-         if(remaining > 0) {
-            Object array = new Object[remaining];
-            
-            for(int i = 0; i < remaining; i++) {
-               try {
-                  Array.set(array, i, list[i + start]);
-               } catch(Exception e){
-                  throw new IllegalStateException("Invalid argument at " + i + " for" + signature, e);
-               }
-            }
-            list[start] = array;
-         }
-         list = Arrays.copyOf(list, length);
-      }
+      Object[] arguments = aligner.align(list); // combine variable arguments to a single array
+
       // create a class scope from the passed in object!!!!!
       if(object == null) {
          object=scope; // XXX tihs is for super only!!!
       }
-      Module module = scope.getModule();
       Scope inner = object.getScope();
       
-      for(int i = 0; i < list.length; i++) {
+      for(int i = 0; i < arguments.length; i++) {
          Type require = types.get(i);
          String name = names.get(i);
-         Object value = list[i];
+         Object argument = arguments[i];
          
-         if(require != null) {
-            Class type = value.getClass();
-            Type actual = module.getType(type);
-            
-            if(require != actual) {
-               List<Type> compatible = actual.getTypes();
-               
-               if(!compatible.contains(require)) {
-                  throw new IllegalArgumentException("Incompatible type for " + signature);
-               }
-            }
+         if(!checker.compatible(scope, argument, require)) {
+            throw new IllegalStateException("Parameter '" + name + "' does not match '" + require + "' for '" + function + "'");
          }
-         Reference reference = new Reference(value);         
+         Reference reference = new Reference(argument);         
          inner.addVariable(name, reference);
       }
       return statement.execute(inner);
