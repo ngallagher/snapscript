ca94841956a96094a2f9d155d35878bb1fa821ba
Niall Gallagher <gallagher_niall@yahoo.com>
2016-02-14 16:16:03 +0000

    Enable command line debugging

---
+++ platform/snap-engine/src/main/java/org/snapscript/engine/ProcessEngineScript.java
@@ -0,0 +1,10 @@
+package org.snapscript.engine;
+
+// 1) start engine container on ephemeral port
+// 2) ensure stdout/stderr goes to both client and console
+// 3) once server is running launch the agent with agent-pool=0
+// 4) as soon as the agent is running, issue an ExecuteCommand
+// 5) once connected the process should suspend
+public class ProcessEngineScript {
+
+}
