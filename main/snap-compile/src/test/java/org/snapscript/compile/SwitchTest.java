db0e595c7d6fd7edc404b60ef0ee98c3a6154115
Niall Gallagher <gallagher_niall@yahoo.com>
2016-04-10 23:51:05 +0100

    Add break test

--- main/snap-compile/src/test/java/org/snapscript/compile/SwitchTest.java
+++ main/snap-compile/src/test/java/org/snapscript/compile/SwitchTest.java
@@ -36,17 +36,44 @@ public class SwitchTest extends TestCase {
    "   }\n"+
    "}\n"+
    "\n"+
-   "testSwitch();\n";   
+   "testSwitch();\n"; 
+   
+   private static final String SOURCE_3 =
+   "function testSwitch() {\n"+
+   "   var x =\"blah\";\n"+
+   "   switch(x){\n"+
+   "   case \"blah\":\n"+
+   "      println(\"blah\");\n"+
+   "      break;\n"+
+   "   case \"test\":\n"+
+   "      println(\"test\");\n"+
+   "   case \"foo\":\n"+
+   "      println(\"foo\");\n"+
+   "   default:\n"+
+   "      println(\"default\");"+
+   "   }\n"+
+   "}\n"+
+   "\n"+
+   "testSwitch();\n"; 
          
    public void testSwitch() throws Exception {
       Compiler compiler = ClassPathCompilerBuilder.createCompiler();
       Executable executable = compiler.compile(SOURCE_1);
+      System.err.println(SOURCE_1);
       executable.execute();
    }   
    
    public void testSwitchWithDefault() throws Exception {
       Compiler compiler = ClassPathCompilerBuilder.createCompiler();
       Executable executable = compiler.compile(SOURCE_2);
+      System.err.println(SOURCE_2);
+      executable.execute();
+   }
+   
+   public void testSwitchWithBreak() throws Exception {
+      Compiler compiler = ClassPathCompilerBuilder.createCompiler();
+      Executable executable = compiler.compile(SOURCE_3);
+      System.err.println(SOURCE_3);
       executable.execute();
    }
 }
