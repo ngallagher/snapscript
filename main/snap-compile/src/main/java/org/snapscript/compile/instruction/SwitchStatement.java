d6145e70a7651e41f4f625cecba9ba12dd27d15c
Niall Gallagher <gallagher_niall@yahoo.com>
2016-04-11 00:15:13 +0100

    Add better debugging for switch

--- main/snap-compile/src/main/java/org/snapscript/compile/instruction/SwitchStatement.java
+++ main/snap-compile/src/main/java/org/snapscript/compile/instruction/SwitchStatement.java
@@ -1,75 +1,102 @@
 package org.snapscript.compile.instruction;
 
+import org.snapscript.core.Compilation;
+import org.snapscript.core.Context;
 import org.snapscript.core.Evaluation;
+import org.snapscript.core.Module;
 import org.snapscript.core.Result;
 import org.snapscript.core.ResultType;
 import org.snapscript.core.Scope;
 import org.snapscript.core.Statement;
+import org.snapscript.core.Trace;
+import org.snapscript.core.TraceInterceptor;
+import org.snapscript.core.TraceStatement;
+import org.snapscript.core.TraceType;
 import org.snapscript.core.Value;
+import org.snapscript.core.error.ErrorHandler;
 
-public class SwitchStatement extends Statement {
-
-   private final Evaluation condition;
-   private final Case[] cases;
+public class SwitchStatement implements Compilation {
    
-   public SwitchStatement(Evaluation condition, Case... cases) {
-      this.condition = condition;
-      this.cases = cases;
+   private final Statement statement;
+   
+   public SwitchStatement(Evaluation evaluation, Case... cases) {
+      this.statement = new CompileResult(evaluation, cases);
    }
    
    @Override
-   public Result compile(Scope scope) throws Exception {
-      Result last = ResultType.getNormal();
+   public Statement compile(Module module, int line) throws Exception {
+      Context context = module.getContext();
+      ErrorHandler handler = context.getHandler();
+      TraceInterceptor interceptor = context.getInterceptor();
+      Trace trace = TraceType.getNormal(module, line);
       
-      for(int i = 0; i < cases.length; i++){
-         Statement statement = cases[i].getStatement();
-         Result result = statement.compile(scope);
-         
-         if(!result.isNormal()){
-            return result;
-         }
-         last = result;
+      return new TraceStatement(interceptor, handler, statement, trace);
+   }
+   
+   private static class CompileResult extends Statement {
+
+      private final Evaluation condition;
+      private final Case[] cases;
+      
+      public CompileResult(Evaluation condition, Case... cases) {
+         this.condition = condition;
+         this.cases = cases;
       }
-      return last;
-   }
-   
-   @Override
-   public Result execute(Scope scope) throws Exception {
-      Value value = condition.evaluate(scope, null);
-      Object left = value.getValue();
       
-      for(int i = 0; i < cases.length; i++){
-         Evaluation evaluation = cases[i].getEvaluation();
+      @Override
+      public Result compile(Scope scope) throws Exception {
+         Result last = ResultType.getNormal();
          
-         if(evaluation == null) {
+         for(int i = 0; i < cases.length; i++){
             Statement statement = cases[i].getStatement();
-            Result result = statement.execute(scope);
+            Result result = statement.compile(scope);
             
-            if(result.isBreak()) {
-               return ResultType.getNormal();
+            if(!result.isNormal()){
+               return result;
             }
-            if(!result.isNormal()) {
-               return result;      
-            }
-            return ResultType.getNormal();
+            last = result;
          }
-         Value other = evaluation.evaluate(scope, null);
-         Object right = other.getValue();
-
-         if(left.equals(right)) {
-            for(int j = i; j < cases.length; j++) {
-               Statement statement = cases[j].getStatement();
+         return last;
+      }
+      
+      @Override
+      public Result execute(Scope scope) throws Exception {
+         Value value = condition.evaluate(scope, null);
+         Object left = value.getValue();
+         
+         for(int i = 0; i < cases.length; i++){
+            Evaluation evaluation = cases[i].getEvaluation();
+            
+            if(evaluation == null) {
+               Statement statement = cases[i].getStatement();
                Result result = statement.execute(scope);
-
+               
                if(result.isBreak()) {
                   return ResultType.getNormal();
                }
                if(!result.isNormal()) {
                   return result;      
                }
-            }   
-         }  
+               return ResultType.getNormal();
+            }
+            Value other = evaluation.evaluate(scope, null);
+            Object right = other.getValue();
+   
+            if(left.equals(right)) {
+               for(int j = i; j < cases.length; j++) {
+                  Statement statement = cases[j].getStatement();
+                  Result result = statement.execute(scope);
+   
+                  if(result.isBreak()) {
+                     return ResultType.getNormal();
+                  }
+                  if(!result.isNormal()) {
+                     return result;      
+                  }
+               }   
+            }  
+         }
+         return ResultType.getNormal();
       }
-      return ResultType.getNormal();
    }
 }
