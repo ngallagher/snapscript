48b9163bb1a3169f513a26c86dadc752ff96f790
Niall Gallagher <gallagher_niall@yahoo.com>
2016-04-14 22:23:12 +0100

    Clean up imports

--- develop/snap-develop/src/main/java/org/snapscript/develop/BackupManager.java
+++ develop/snap-develop/src/main/java/org/snapscript/develop/BackupManager.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
-import java.nio.file.Files;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
