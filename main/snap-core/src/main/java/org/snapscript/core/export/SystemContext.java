3dc62b1d71478c1121eab19ed49a3e9eece9212f
Niall Gallagher <gallagher_niall@yahoo.com>
2016-05-28 15:42:59 +0100

    Return the module loaded

--- main/snap-core/src/main/java/org/snapscript/core/export/SystemContext.java
+++ main/snap-core/src/main/java/org/snapscript/core/export/SystemContext.java
@@ -3,6 +3,7 @@ package org.snapscript.core.export;
 import org.snapscript.core.Context;
 import org.snapscript.core.ExpressionEvaluator;
 import org.snapscript.core.Module;
+import org.snapscript.core.ModuleRegistry;
 import org.snapscript.core.Package;
 import org.snapscript.core.Scope;
 import org.snapscript.core.Statement;
@@ -24,12 +25,15 @@ public class SystemContext {
       return executor.evaluate(scope, source, name);
    }
    
-   public void load(Scope scope, String name) throws Exception {
+   public Module load(Scope scope, String name) throws Exception {
+      ModuleRegistry registry = context.getRegistry();
       TypeLoader loader = context.getLoader();
       Package module = loader.importPackage(name);
       Statement statement = module.compile(scope);
       
       statement.execute(scope);
+      
+      return registry.getModule(name);
    }
    
    public void printf(Scope scope, Object value, Object... values)  throws Exception{
